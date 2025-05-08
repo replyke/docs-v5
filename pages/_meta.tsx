6a57513406c51a902bc14411e03cff7dca4b7c83
Yanay Zabary <yanay.tsabary@gmail.com>
2025-05-08 14:08:33 +0300

    add nav to old docs

--- pages/_meta.tsx
+++ pages/_meta.tsx
@@ -7,6 +7,16 @@ export default {
   "react-and-react-native": "React & React Native",
   "api-endpoints": "API Endpoints",
   "data-models": "Data Models",
+  versions: {
+    type: "menu",
+    title: "Versions",
+    items: {
+      _4: {
+        title: "Replyke v4 Docs",
+        href: "https://v4.docs.replyke.com",
+      },
+    },
+  },
   contact: {
     title: "Contact ↗",
     type: "page",
