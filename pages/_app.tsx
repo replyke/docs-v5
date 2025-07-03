ff6cd3bd2d8afed314930003e3ae827195bde529
Yanay Zabary <yanay.tsabary@gmail.com>
2025-07-03 12:30:16 +0200

    added analyics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,30 @@
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
         <link rel="icon" href="/favicon.webp" type="image/webp" />
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-R0WW1RW0XF"
+          strategy="afterInteractive"
+        />
+        <Script
+          id="gtag-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-R0WW1RW0XF', {
+                cookie_domain: 'replyke.com'
+              });
+            `,
+          }}
+        />
       </Head>
       <Component {...pageProps} />
     </>
